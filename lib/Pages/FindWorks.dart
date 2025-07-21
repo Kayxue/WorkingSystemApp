fdf2215d30821edfa10bfcccc66ce015a4800a12
Kayxue <s0990006@gmail.com>
2025-07-21 23:22:34 +0800

    Change listview to infinite scroll pagination

--- lib/Pages/FindWorks.dart
+++ lib/Pages/FindWorks.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:working_system_app/Types/GigPagination.dart';
 import 'package:working_system_app/Types/Gigs.dart';
 import 'package:working_system_app/Types/PublicGigsReturn.dart';
 
@@ -14,107 +14,91 @@ class Findworks extends StatefulWidget {
 }
 
 class _FindworksState extends State<Findworks> {
-  bool isFetching = true;
-  late List<Gigs> gigs;
-  late Gigpagination pagination;
+  String searchQuery = "";
+  late final _pagingController = PagingController<int, Gigs>(
+    getNextPageKey: (state) =>
+        state.lastPageIsEmpty ? null : state.nextIntPageKey,
+    fetchPage: (pageKey) async {
+      final result = await fetchWorks(page: pageKey);
+      return result;
+    },
+  );
 
-  Future<void> fetchInitialWorks() async {
+  Future<List<Gigs>> fetchWorks({int page = 1}) async {
     final response = await http.get(
-      Uri.parse("http://0.0.0.0:3000/gig/public"),
+      Uri.parse(
+        "http://0.0.0.0:3000/gig/public?page=$page${searchQuery.isNotEmpty ? "&search=$searchQuery" : ""}",
+      ),
       headers: {"platform": "mobile"},
     );
-    if (!mounted) return;
+    if (!mounted) return [];
     if (response.statusCode != 200) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text("Failed to fetch works. Please try again.")),
       );
-      return;
+      return [];
     }
     final respond = jsonDecode(response.body) as Map<String, dynamic>;
     final parsed = Publicgigsreturn.fromJson(respond);
-    // print(parsed.toJson());
-    setState(() {
-      gigs = parsed.gigs;
-      pagination = parsed.pagination;
-    });
-    if (gigs.isEmpty) {
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text("No works found.")));
-    }
+    return parsed.gigs;
   }
 
   @override
-  void initState() {
-    super.initState();
-    fetchInitialWorks().then((_) {
-      setState(() {
-        isFetching = false;
-      });
-    });
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return isFetching
-        ? Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircularProgressIndicator(),
-                SizedBox(height: 16),
-                Text("Loading", style: TextStyle(fontSize: 16)),
-              ],
+    return Padding(
+      padding: EdgeInsetsGeometry.only(top: 16, right: 16, left: 16, bottom: 8),
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hint: Text("Search works", style: TextStyle(color: Colors.grey)),
+              border: OutlineInputBorder(),
             ),
-          )
-        : Padding(
-            padding: EdgeInsetsGeometry.only(top: 16, right: 16, left: 16),
-            child: Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    hint: Text(
-                      "Search works",
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                Expanded(
-                  child: RefreshIndicator(
-                    onRefresh: fetchInitialWorks,
-                    child: ListView.builder(
-                      itemCount: gigs.length,
-                      itemBuilder: (context, index) {
-                        final work = gigs[index];
-                        return Card(
-                          clipBehavior: Clip.hardEdge,
-                          child: InkWell(
-                            splashColor: Colors.grey.withAlpha(30),
-                            onTap: () {
-                              //TODO: Show gig details
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text(
-                                    "Tapped on Work Title ${work.title}",
-                                  ),
-                                ),
-                              );
-                            },
-                            child: ListTile(
-                              title: Text(work.title),
-                              trailing: Text("\$${work.hourlyRate}"),
-                              subtitle: Text("${work.city} ${work.district}")
+          ),
+          SizedBox(height: 16),
+          PagingListener(
+            controller: _pagingController,
+            builder: (context, state, fetchNextPage) => Expanded(
+              child: RefreshIndicator(
+                onRefresh: () => Future.sync(() => _pagingController.refresh()),
+                child: PagedListView<int, Gigs>(
+                  state: state,
+                  fetchNextPage: fetchNextPage,
+                  builderDelegate: PagedChildBuilderDelegate(
+                    itemBuilder: (context, item, index) => Card(
+                      clipBehavior: Clip.hardEdge,
+                      child: InkWell(
+                        splashColor: Colors.grey.withAlpha(30),
+                        onTap: () {
+                          //TODO: Show gig details
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                "Tapped on Work Title ${item.title}",
+                              ),
                             ),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                        child: ListTile(
+                          title: Text(item.title),
+                          trailing: Text("\$${item.hourlyRate}"),
+                          subtitle: Text("${item.city} ${item.district}"),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ],
+              ),
             ),
-          );
+          ),
+        ],
+      ),
+    );
   }
 }
