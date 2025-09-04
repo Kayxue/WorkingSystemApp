2c82bf54e979926da16b4c11e1267dea5bd3cb75
Kayxue <s0990006@gmail.com>
2025-09-04 18:13:39 +0800

    Add user rating

--- lib/Pages/Personal.dart
+++ lib/Pages/Personal.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:rhttp/rhttp.dart';
 import 'package:working_system_app/Others/Utils.dart';
 import 'package:working_system_app/Types/WorkerProfile.dart';
@@ -23,7 +26,7 @@ class _PersonalState extends State<Personal> {
   late WorkerProfile profile;
   bool isLoading = true;
 
-  Future<void> loadUserProfile() async {
+  Future<WorkerProfile> loadUserProfile() async {
     final response = await Utils.client.get(
       "/user/profile",
       headers: HttpHeaders.rawMap({
@@ -35,68 +38,104 @@ class _PersonalState extends State<Personal> {
       widget.updateIndex(1);
       widget.clearSessionKey();
     }
-    final Map<String, dynamic> respond = response.bodyToJson();
-    setState(() {
-      profile = WorkerProfile.fromJson(respond);
-      isLoading = false;
+    final Map<String, dynamic> respond = jsonDecode(response.body);
+    return WorkerProfile.fromJson(respond);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadUserProfile().then((profile) {
+      setState(() {
+        this.profile = profile;
+        isLoading = false;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Personal",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Card(
-              color: Colors.blue,
-              child: Container(
-                width: double.infinity,
-                height: 100,
-                padding: EdgeInsets.symmetric(vertical: 4.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 16),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+      child: isLoading
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(height: 8),
+                  Text("Loading", style: TextStyle(fontSize: 12)),
+                ],
+              ),
+            )
+          : Padding(
+              padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Personal",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: double.infinity,
+                      height: 100,
+                      padding: EdgeInsets.symmetric(vertical: 4.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            "洛陽停車場",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 4),
-                          Text(
-                            "台北市萬華區環河南路一段1號",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
+                          Padding(
+                            padding: EdgeInsets.only(left: 16),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "${profile.lastName}${profile.firstName}",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 4),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Your rating:",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    SizedBox(width: 4),
+                                    RatingBarIndicator(
+                                      rating: profile.ratingStats.averageRating,
+                                      itemBuilder: (context, index) => Icon(
+                                        Icons.star,
+                                        color: Colors.amber,
+                                      ),
+                                      itemCount: 5,
+                                      itemSize: 16.0,
+                                      direction: Axis.horizontal,
+                                    )
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
                         ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
