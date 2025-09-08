05a6fe010b28f05cdc14a552fd8caec19aaab2d6
Kayxue <s0990006@gmail.com>
2025-09-08 17:01:27 +0800

    Init update profile page

---
+++ lib/Pages/UpdateUserInfo.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class UpdateUserInfo extends StatefulWidget {
+  final String sessionKey;
+  final Function clearSessionKey;
+  final Function(int) updateIndex;
+
+  const UpdateUserInfo({
+    super.key,
+    required this.sessionKey,
+    required this.clearSessionKey,
+    required this.updateIndex,
+  });
+
+  @override
+  State<UpdateUserInfo> createState() => _UpdateUserInfoState();
+}
+
+class _UpdateUserInfoState extends State<UpdateUserInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Update Information')),
+      body: Padding(padding: const EdgeInsets.all(16.0), child: Column(
+        children: [
+          TextField(
+            decoration: const InputDecoration(labelText: 'Uername'),
+          ),
+        ],
+      )),
+    );
+  }
+}
