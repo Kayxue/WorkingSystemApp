8ea82206f960c1e030f883d0232f954fd315ed94
Kayxue <s0990006@gmail.com>
2025-10-17 20:38:52 +0800

    Building all things in Java 21

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,13 +15,9 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-// Configure Java version for all subprojects to suppress obsolete warnings
-subprojects {
+    
+    // Configure Java version for all subprojects to suppress obsolete warnings
+    // This must be registered before evaluationDependsOn is called
     afterEvaluate {
         if (project.hasProperty("android")) {
             extensions.findByType<com.android.build.gradle.BaseExtension>()?.apply {
@@ -39,6 +35,10 @@ subprojects {
             }
         }
     }
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
