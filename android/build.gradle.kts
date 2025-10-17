f726484728b99525d30fce4337c89d0cd937c47a
Kayxue <s0990006@gmail.com>
2025-10-17 20:31:26 +0800

    Building all things in Java 21

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,6 +20,27 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Configure Java version for all subprojects to suppress obsolete warnings
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            extensions.findByType<com.android.build.gradle.BaseExtension>()?.apply {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_21
+                    targetCompatibility = JavaVersion.VERSION_21
+                }
+            }
+        }
+        
+        // For Kotlin projects
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            kotlinOptions {
+                jvmTarget = "21"
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
