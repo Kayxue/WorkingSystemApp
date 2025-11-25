d67cd99a1063589f5252d34316196eb7b081def7
Kayxue <s0990006@gmail.com>
2025-11-26 04:44:18 +0800

    Fix login screen overflow

--- lib/Pages/login.dart
+++ lib/Pages/login.dart
@@ -96,135 +96,152 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: .only(left: 24, right: 24, top: 16),
-      child: Column(
-        mainAxisAlignment: .center,
-        children: [
-          Row(
-            mainAxisAlignment: .center,
-            children: [
-              Text("Login", style: TextStyle(fontSize: 24, fontWeight: .bold)),
-            ],
-          ),
-          SizedBox(height: 16),
-          TextField(
-            decoration: InputDecoration(
-              labelText: "Email",
-              border: OutlineInputBorder(),
-            ),
-            onChanged: (value) => setState(() {
-              email = value;
-            }),
-            keyboardType: .emailAddress,
-          ),
-          SizedBox(height: 16),
-          TextField(
-            decoration: InputDecoration(
-              labelText: "Password",
-              border: OutlineInputBorder(),
-            ),
-            onChanged: (value) => setState(() {
-              password = value;
-            }),
-            obscureText: true,
-            keyboardType: TextInputType.visiblePassword,
-          ),
-          SizedBox(height: 16),
-          TextField(
-            controller: captchaController,
-            decoration: InputDecoration(
-              labelText: "Captcha Code",
-              border: OutlineInputBorder(),
-            ),
-            onChanged: (value) => setState(() {
-              captchaCode = value;
-            }),
-            keyboardType: TextInputType.text,
-          ),
-          SizedBox(height: 16),
-          if (captchaImage != null && captchaAnswer?.isNotEmpty == true)
-            SizedBox(
-              height: 100,
-              child: Row(
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minHeight: constraints.maxHeight),
+            child: Padding(
+              padding: .only(left: 24, right: 24, top: 16),
+              child: Column(
                 mainAxisAlignment: .center,
                 children: [
-                  Image.memory(captchaImage!),
-                  SizedBox(width: 8),
-                  IconButton(
-                    onPressed: fetchCaptcha,
-                    icon: Icon(Icons.refresh),
+                  Row(
+                    mainAxisAlignment: .center,
+                    children: [
+                      Text(
+                        "Login",
+                        style: TextStyle(fontSize: 24, fontWeight: .bold),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (value) => setState(() {
+                      email = value;
+                    }),
+                    keyboardType: .emailAddress,
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (value) => setState(() {
+                      password = value;
+                    }),
+                    obscureText: true,
+                    keyboardType: TextInputType.visiblePassword,
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: captchaController,
+                    decoration: InputDecoration(
+                      labelText: "Captcha Code",
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (value) => setState(() {
+                      captchaCode = value;
+                    }),
+                    keyboardType: TextInputType.text,
+                  ),
+                  SizedBox(height: 16),
+                  if (captchaImage != null && captchaAnswer?.isNotEmpty == true)
+                    SizedBox(
+                      height: 100,
+                      child: Row(
+                        mainAxisAlignment: .center,
+                        children: [
+                          Image.memory(captchaImage!),
+                          SizedBox(width: 8),
+                          IconButton(
+                            onPressed: fetchCaptcha,
+                            icon: Icon(Icons.refresh),
+                          ),
+                        ],
+                      ),
+                    ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: FilledButton(
+                          onPressed: login,
+                          child: Text("Login"),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: .center,
+                    children: [
+                      Text("Don't have an account?"),
+                      SizedBox(width: 8),
+                      GestureDetector(
+                        onTap: () async {
+                          final result = await Navigator.of(context).push<bool>(
+                            MaterialPageRoute(builder: (context) => Register()),
+                          );
+                          if (result == true) {
+                            if (!context.mounted) return;
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  "Registration successful. Please log in.",
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                        child: Text(
+                          "Register",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 4),
+                      Text("|"),
+                      SizedBox(width: 4),
+                      GestureDetector(
+                        onTap: () async {
+                          final result = await Navigator.of(context).push<bool>(
+                            MaterialPageRoute(
+                              builder: (context) => ResetEnterEmail(),
+                            ),
+                          );
+                          if (result == true) {
+                            if (!context.mounted) return;
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  "Password reset successful. Please log in with your new password.",
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                        child: Text(
+                          "Reset Password",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
-          SizedBox(height: 16),
-          Row(
-            children: [
-              Expanded(
-                child: FilledButton(onPressed: login, child: Text("Login")),
-              ),
-            ],
           ),
-          SizedBox(height: 16),
-          Row(
-            mainAxisAlignment: .center,
-            children: [
-              Text("Don't have an account?"),
-              SizedBox(width: 8),
-              GestureDetector(
-                onTap: () async {
-                  final result = await Navigator.of(context).push<bool>(
-                    MaterialPageRoute(builder: (context) => Register()),
-                  );
-                  if (result == true) {
-                    if (!context.mounted) return;
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(
-                          "Registration successful. Please log in.",
-                        ),
-                      ),
-                    );
-                  }
-                },
-                child: Text(
-                  "Register",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-                ),
-              ),
-              SizedBox(width: 4),
-              Text("|"),
-              SizedBox(width: 4),
-              GestureDetector(
-                onTap: () async {
-                  final result = await Navigator.of(context).push<bool>(
-                    MaterialPageRoute(builder: (context) => ResetEnterEmail()),
-                  );
-                  if (result == true) {
-                    if (!context.mounted) return;
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(
-                          "Password reset successful. Please log in with your new password.",
-                        ),
-                      ),
-                    );
-                  }
-                },
-                child: Text(
-                  "Reset Password",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
